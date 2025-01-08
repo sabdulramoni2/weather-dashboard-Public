cc5b2b2301cafcc7f1acac212619ac07d6853991
saidi <abdulramonisaidi@yahoo.com>
2025-01-07 23:32:24 -0500

    added names of cities

--- src/weather_dashboard.py
+++ src/weather_dashboard.py
@@ -73,7 +73,7 @@ def main():
     # Create bucket if needed
     dashboard.create_bucket_if_not_exists()
     
-    cities = ["Philadelphia", "Seattle", "New York"]
+    cities = ["Atlanta", "Chicago", "Mephis"]
     
     for city in cities:
         print(f"\nFetching weather for {city}...")
